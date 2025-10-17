efa6123b390138b0f21d6fcc26d605463ea27d81
IgnacioWow <ignaciowilliams987mail.com>
2025-10-17 17:21:42 -0300

    Versión 1.0 + README

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -2,8 +2,21 @@ import type { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
   appId: 'io.ionic.starter',
-  appName: 'todo-aiep',
-  webDir: 'www'
+  appName: 'ToDoApp', 
+  webDir: 'www',
+  server: {
+    androidScheme: 'https'
+  },
+  plugins: {
+    SplashScreen: {
+      launchShowDuration: 3000, // Duración en milisegundos (3 segundos)
+      launchAutoHide: true,     // Se oculta automáticamente al terminar el tiempo
+      backgroundColor: "#ffffff", // Pon aquí el color de fondo de tu imagen splash
+      androidSplashResourceName: "splash", // No cambies este nombre
+      androidScaleType: "CENTER_CROP", // Cómo se ajusta la imagen
+      showSpinner: false, // Oculta la ruedita de carga
+    }
+  }
 };
 
 export default config;
